541fef59ef90c692a4de0c0aeb520ee6c4d95ed6
litvin0d <litvinovd944@gmail.com>
2023-04-21 04:20:44 +0500

    Remove all files from main branch

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-
-import App from "./App.vue";
-import "./assets/main.css";
-import { store } from "./store";
-
-createApp(App)
-    .use(store)
-    .mount("#app");
